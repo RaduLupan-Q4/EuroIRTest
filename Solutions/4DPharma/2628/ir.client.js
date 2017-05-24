571b551cfedffc1e4207101d59a4e0f6a339b8b3
RaduLupan-Q4 <radul@q4inc.com>
2017-05-24 16:53:03 -0400

    Update Solutions folder.

---
+++ Solutions/4DPharma/2628/ir.client.js
@@ -0,0 +1,10 @@
+﻿var clientStyleOverwrite = new function ()
+{
+    this.chart_ColourMain = '#06083e';
+    this.chart_ColourBackground = '#fff';
+    this.chart_ColourBorder = '#EEEEEE';
+    this.formatDate = 'DD-MM-YYYY';
+    this.formatTime = "HH:mm";
+    this.formatDateTime = "DD-MM-YYYY HH:mm";
+    this.chart_DrawMode = "line";
+}
