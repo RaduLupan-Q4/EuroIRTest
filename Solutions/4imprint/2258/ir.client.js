595c7b036e381134434cffd2860b022bf1ee4606
RaduLupan-Q4 <radul@q4inc.com>
2017-06-03 20:34:01 -0400

    Added more clients.

---
+++ Solutions/4imprint/2258/ir.client.js
@@ -0,0 +1,21 @@
+﻿var clientStyleOverwrite = new function () {
+    this.chart_ColourBackground = '#f9f9f9';
+    this.chart_ColourBorder = '#e9e9e9';
+    this.amountOfDecimals = 2;
+    this.formatDate = 'DD-MM-YYYY';
+    this.formatTime = 'HH:mm';
+    this.formatDateTime = 'DD-MM-YYYY HH:mm';
+    this.news_lockLCID = 2057;
+}
+
+var clientRNSFilters = [
+    { name: 'Acquisition and Alliances', categories: 'ACQ;CNT' },
+    { name: 'Annual Financial Report', categories: 'ACS' },
+    { name: 'AGM Statement', categories: 'AGM' },
+    { name: 'RE Agreement', categories: 'AGR' },
+    { name: 'Change of Adviser', categories: 'APP' },
+    { name: 'Directorate Change', categories: 'BOA;MSC' },
+    { name: 'Disposal', categories: 'DIS' },
+    { name: 'Holding(s) in Company', categories: 'HOL' },
+    { name: 'Miscellaneous', categories: 'MSC' }
+];
